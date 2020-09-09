0e7311a91a6034044092b8be306cb2251de2d50f
Marianna <mamonteiro.oliveira@gmail.com>
2020-09-09 16:23:46 -0300

    Update

--- funcao/ParamPadrao.js
+++ funcao/ParamPadrao.js
@@ -10,5 +10,16 @@ console.log(soma1(), soma1(3), soma1(1, 2, 3),soma1(0,0,0)) // zero retorna fals
 // estrategia2, 3 e 4 para gerar valor padrao
 function soma2(a, b, c) {
     a = a !== undefined ? a : 1
-    b = 1 in arguments
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+    return a + b + c 
 }
+
+console.log(soma2(), soma2(3), soma2(1, 2, 3 ), soma2(0, 0, 0))
+
+// valro padrao ES2015
+function soma3(a = 1, b = 1, c = 1) {
+    return a + b + c
+}
+
+console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0,0,0))
