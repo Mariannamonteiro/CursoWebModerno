bc30f105b7e0118d58f3b2ee96c81e060bb7c9c8
Marianna <mamonteiro.oliveira@gmail.com>
2020-09-16 16:57:30 -0300

    Introdução à OO.

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// código não executável !!
+
+// Procedural 
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto ={
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto 
+
+// Principios importantes
+//1. Abstracao
+//2. encapsulamento 
+//3. herança (prototype)
+//4.polimorfismo
