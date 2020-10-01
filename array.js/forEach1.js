377c694d5a6c41eb29890b26ab8d1da93e741d80
Marianna <mamonteiro.oliveira@gmail.com>
2020-10-01 17:48:06 -0300

    Update and New Lessons

--- array.js/forEach1.js
+++ array.js/forEach1.js
@@ -3,7 +3,7 @@ const aprovados = ['Agatha','Aldo', 'Daniel', 'Raquel']
 
 aprovados.forEach(function(nome, indice, array){
     console.log(`${indice+1}) ${nome} `)
-    console.log(array )
+    
 })
 
 aprovados.forEach(nome => console.log(nome))
